5dc1aa9b94c7a7981b84a8cb2dc5f7915a73c80d
damarabc <damaradamara57@gmail.com>
2026-09-15 08:49:39 +0700

    feat: api ticket

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\TicketController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,4 +12,11 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::get('/dashboard/statistics', [DashboardController::class, 'getStatistics']);
+
+    Route::get('/ticket', [TicketController::class, 'index']);
+    Route::get('/ticket/{code}', [TicketController::class, 'show']);
+    Route::post('/ticket', [TicketController::class, 'store']);
+    Route::post('/ticket-reply/{code}', [TicketController::class, 'storeReply']);
 });
